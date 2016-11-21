efa0501a93e40d948f79015ee5c735724069299d
Wesley W Werneck <wesley@simbioseventures.com>
2016-11-21 16:54:22 +0000

    Get sleep time by enviroment variable

--- tests_and_examples/run_query_tests.py
+++ tests_and_examples/run_query_tests.py
@@ -40,7 +40,7 @@ class SlicingDiceTester(object):
         # Translation table for fields with timestamp
         self.field_translation = {}
 
-        self.sleep_time = 3  # seconds
+        self.sleep_time = os.environ.get("CLIENT_SLEEP_TIME", 5)  # seconds
         self.path = 'examples/'  # Directory containing examples to test
         self.extension = '.json'  # Examples file format
 
